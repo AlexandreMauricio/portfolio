ffc16a3db34c3178e48e05333785641d67496100
guita <guita@LAPTOP-EJGFIKDL.lan>
2021-04-24 21:09:38 +0100

    Base da Página principal

---
+++ PPP.php
@@ -0,0 +1,83 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>AM || Estudante Eng.I</title>
+<link href="./estilo.css" type="text/css" rel="stylesheet">
+</head>
+<style>
+@import
+	url('https://fonts.googleapis.com/css2?family=Architects+Daughter&display=swap')
+	;
+
+@import
+	url('https://fonts.googleapis.com/css2?family=Coda+Caption:wght@800&display=swap')
+	;
+
+@import
+	url('https://fonts.googleapis.com/css2?family=Montserrat&display=swap')
+	;
+
+body {
+	background-color: #1E1E1E;
+}
+
+ul {
+	list-style-type: none;
+	margin: 0;
+	padding: 0;
+	width: 150px;
+	background-color: #161616;
+	height: 1000px;
+}
+
+li a {
+	display: block;
+	color: #000;
+	padding: 8px 16px;
+	text-decoration: none;
+	color: gray;
+}
+
+/* Change the link color on hover */
+li a:hover {
+	background-color: #555;
+	color: white;
+}
+
+xmp {
+	font-family: 'ArchitectsDaughter', cursive;
+	color: gray
+}
+</style>
+<body>
+	<ul style="float: left">
+		<li><img src="logo.jpg" alt="Me :)" style="width: 150px; height: auto"></li>
+		<li><a href="#home">Home</a></li>
+		<li><a href="#news">News</a></li>
+		<li><a href="#contact">Contact</a></li>
+		<li><a href="#about">About</a></li>
+	</ul>
+	<i><xmp>
+		<body>	
+		</xmp> </i>
+	<i><xmp>
+		<h1>	
+		</xmp> </i>
+	<h1
+		style="color: white; font-family: 'Coda Caption', sans-serif; text-align: center;">
+		Ola, sou o Alexandre Mauricio,<br> estudante de Engenharia Informatica
+	</h1>
+	<i><xmp>
+		</h1>
+		</xmp> </i>
+	<i><xmp>
+		<p>
+		</xmp> </i>
+	<p
+		style="font-family: 'Montserrat', sans-serif; color: gray; text-align: center;">Estudante
+		de Engenharia Informatica no IPCB</p>
+	<i><xmp>
+		</p>
+		</xmp> </i>
+</body>
+</html>
